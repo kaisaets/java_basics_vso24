c88491b0c18320f78333db34a2fe7d0d59f3a3fd
kaisa.ets <kaisaets@gmail.com>
2025-09-10 11:08:57 +0300

    ex 88.1 and 88.2

---
+++ src/week5/ex88/Main.java
@@ -0,0 +1,28 @@
+package week5.ex88;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner newStudent = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        while (true) {
+            System.out.println("name: ");
+            String name = newStudent.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.println("studentnumber: ");
+            String studentNumber = newStudent.nextLine();
+
+            Student student = new Student(name, studentNumber);
+            students.add(student);
+        }
+        System.out.println("list of students: ");
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+}
