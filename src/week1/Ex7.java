a8d1ea27ed6c4f19e7fef36caf6b46e5ac84e01b
kaisa.ets <kaisaets@gmail.com>
2025-09-15 14:17:23 +0300

    ex 7

---
+++ src/week1/Ex7.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 8;
+        int c = 277;
+        int d = 111;
+        int result1 = a * b;
+        int result2 = c * d;
+
+        System.out.println(a + " * " + b + " = " + result1);
+        System.out.println(c + " * " + d + " = " + result2);
+    }
+}
