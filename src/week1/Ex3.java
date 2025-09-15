0ba20b5ee3e83acf536b6cbf84d518ae0fa1713a
kaisa.ets <kaisaets@gmail.com>
2025-09-15 13:42:29 +0300

    ex 3

---
+++ src/week1/Ex3.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+
+    }
+}
