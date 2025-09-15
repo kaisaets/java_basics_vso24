efe77586884f9e2c38c267ee3789ddd80b11ce39
kaisa.ets <kaisaets@gmail.com>
2025-09-15 13:57:23 +0300

    ex 4

---
+++ src/week1/Ex4.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        int chickens = 9000;
+        double bacon = 0.1;
+        String tractor = "Zetor";
+
+        System.out.println("Chickens:");
+        System.out.println(chickens);
+        System.out.println("Bacon (kg):");
+        System.out.println(bacon);
+        System.out.println("A tractor:");
+        System.out.println(tractor);
+        System.out.println("In a nutshell:");
+        System.out.println();
+        System.out.println(chickens);
+        System.out.println(bacon);
+        System.out.println(tractor);
+    }
+}
