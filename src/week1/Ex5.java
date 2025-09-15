f56d4453ce06d1a10a12c6f360afa0e2bd0b57a5
kaisa.ets <kaisaets@gmail.com>
2025-09-15 14:03:34 +0300

    ex 5

---
+++ src/week1/Ex5.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int daysInAYear = 365;
+        int secondsInADay  = 24 * 60 * 60;
+        int secondsInAYear = daysInAYear * secondsInADay;
+        System.out.println("There are " + secondsInAYear + " seconds in a year.");
+
+    }
+}
