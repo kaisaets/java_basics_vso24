6e81d8ad416bb634bc1b9e107aee49be0f7f859f
kaisa.ets <kaisaets@gmail.com>
2025-09-15 14:32:38 +0300

    ex 8

---
+++ src/week1/Ex8.java
@@ -0,0 +1,15 @@
+package week1;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Ex8 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int firstNumber = Integer.parseInt(reader.nextLine());
+        System.out.println("Type another number: ");
+        int secondNumber = Integer.parseInt(reader.nextLine());
+        System.out.println("Sum of the numbers: " + (firstNumber + secondNumber));
+    }
+}
