533247f2076b8afd97e87984a02aaa72923d1921
kaisa.ets <kaisaets@gmail.com>
2025-09-15 15:23:52 +0300

    Ex 21

---
+++ src/week1/Ex21.java
@@ -0,0 +1,16 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Ex21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = sc.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("Leap year!");
+        } else {
+            System.out.println("Not a leap year!");
+        }
+    }
+}
