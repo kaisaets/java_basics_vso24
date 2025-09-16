30d6c5f2f2801233519e98972b40df721daf657b
kaisa.ets <kaisaets@gmail.com>
2025-09-16 09:24:03 +0300

    Ex 29

---
+++ src/week2/Ex29.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class Ex29 {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number <= 100) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
