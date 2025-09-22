82fbab9fefdcbddda4624709cfea0a5a6aedbd41
kaisa.ets <kaisaets@gmail.com>
2025-09-22 09:57:48 +0300

    Ex 71

---
+++ src/week4/Ex71.java
@@ -0,0 +1,35 @@
+package week4;
+
+import java.util.ArrayList;
+
+public class Ex71 {
+    public static void smartCombine(ArrayList<Integer> first, ArrayList<Integer> second) {
+        for(int number : second){
+            if (!first.contains(number)){
+                first.add(number);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+        list1.add(4);
+        list1.add(3);
+
+        list2.add(5);
+        list2.add(10);
+        list2.add(7);
+        list2.add(3);
+        list2.add(5);
+        list2.add(10);
+
+        smartCombine(list1, list2);
+
+        System.out.println(list1);
+
+        System.out.println(list2);
+    }
+
+}
