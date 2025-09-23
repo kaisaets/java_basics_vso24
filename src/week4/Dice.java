043133144856b58c166b0fab04a5fec4392db6f0
kaisa.ets <kaisaets@gmail.com>
2025-09-23 10:39:55 +0300

    Ex 80

---
+++ src/week4/Dice.java
@@ -0,0 +1,27 @@
+package week4;
+
+import java.util.Random;
+
+public class Dice {
+    private Random random;
+    private int numberOfSides;
+
+    public Dice (int numberOfSides) {
+        this.numberOfSides = numberOfSides;
+        random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(numberOfSides);
+    }
+
+    public static void main(String[] args) {
+        Dice dice = new Dice(6);
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println(dice.roll());
+            i++;
+        }
+    }
+}
